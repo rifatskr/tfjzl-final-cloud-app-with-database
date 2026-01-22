83dfb59d8714c3cf1dcb83ac5902bde4a7c00b29
Rifat Sarkar <rifatskr@users.noreply.github.com>
2026-01-22 18:52:10 -0500

    Task 2: Update admin for exam models

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,30 +1,57 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# Import all 7 models required by the lab
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 
+# ----- Inlines (edit related objects on the same page) -----
+
+# Show lessons inside the Course admin page
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
 
-# Register your models here.
+# Show questions inside the Course admin page
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 2
+
+
+# Show choices inside the Question admin page
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
+
+
+# ----- Admin classes -----
+
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title', 'order', 'course']
+    list_filter = ['course']
+    search_fields = ['title', 'content']
 
 
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'course', 'grade']
+    list_filter = ['course']
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
+
+
+# ----- Register models -----
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
 admin.site.register(Instructor)
 admin.site.register(Learner)
